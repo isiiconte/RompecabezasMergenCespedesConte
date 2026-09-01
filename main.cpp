73ad63848ccda4713b6e58b419662394e181b8b0
Alumno <bellaconte366@gmail.com>
2026-09-01 15:36:45 -0300

    hola

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+#include <conio.h>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int tablero[n][n];
+int vacioX, vacioY;
+void inicializarTablero(int tablero[][]);
+int main()
+{
+
+    int n;
+    cout << "Elegi tamaño de tu rompecabezas (nxn)" << endl;
+    cout << "1. 3x3, 2. 4x4 3.5x5"<<endl;
+    cin >> n;
+
+
+    return 0;
+}
+void inicializarTablero(int tablero[][n])
+{
+
+}
